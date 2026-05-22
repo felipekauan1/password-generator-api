9af7de166dcf201c6e3f7160b643e139dfea51ab
Felipe Kauãn <felipekauan30@outlook.com>
2026-05-22 07:51:15 -0300

    feat: create passwords migration

---
+++ database/migrations/2026_05_22_103726_create_passwords_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('passwords', function (Blueprint $table) {
+            $table->id();
+            $table->text('password');
+            $table->integer('length');
+            $table->boolean('uppercase')->default(false);
+            $table->boolean('lowercase')->default(false);
+            $table->boolean('numbers')->default(false);
+            $table->boolean('symbols')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('passwords');
+    }
+};
